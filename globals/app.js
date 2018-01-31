481805307a383ef7d5810e69e83dc1c391894232
Malachi Partlow <malachipartlow@Malachis-iMac.local>
2018-01-31 13:00:09 -0500

    .

--- globals/app.js
+++ globals/app.js
@@ -1,12 +1,26 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('Degrees', [
         'ngAnimate'
     ])
 
-    .filter('orderSemesters', semester => {
-        $scope.semesterOrder = ["fall", "summer", "winter"]
-        console.log(semester)
-    })
+        .filter('orderSemesters', semester => {
+            $scope.semesterOrder = ["fall", "summer", "winter"]
+            console.log(semester)
+        })
+
+        .directive('fadeIn', function ($timeout) {
+            return {
+                restrict: 'A',
+                link: function ($scope, $element, attrs) {
+                    $element.addClass("ng-hide-remove");
+                    console.log("hi")
+                    $element.on('load', function () {
+                        console.log("bye")
+                        $element.addClass("ng-hide-add");
+                    });
+                }
+            };
+        })
 })();
